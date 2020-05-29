605a140877774eecc31ae0a966179f5975f0ac9e
Wekat <60980395+Wekat@users.noreply.github.com>
2020-05-29 15:59:55 +0200

    Some more chaos

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ var url = 'http://newsapi.org/v2/top-headlines?' +
           'country=us&' +
           'apiKey=abb493012a5a47909c682c381a8e855b';
 
+// const vm = new Vue({
+// el: '#app',
+// data: {
+//     results: [
+//         {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
+//         {title: "and then there was the second", abstract: "lorem ipsum some test dimsum"},
+//         {title: "third time's a charm", abstract: "lorem ipsum some test dimsum"},
+//         {title: "four the last time", abstract: "lorem ipsum some test dimsum"}
+//         ]
+//     }
+// });
+
 
 const app = new Vue({
     el: '#app',
@@ -11,14 +23,10 @@ const app = new Vue({
     },
     
     mounted: function() {
-        fetch('http://localhost:5500/' + url)
+        fetch(url)
             .then(response => response.json())
             .then(articlesResponse => {
                 this.articles = articlesResponse;
             })
     }
 })
-
-fetch('https://cors-anywhere.herokuapp.com/' + url)
- .then(response => response.json())
- .then(json => console.log(json))
