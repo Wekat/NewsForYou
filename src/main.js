51ea5277416d3fdc81f2500d0cdcb7d79d65f67b
Wekat <60980395+Wekat@users.noreply.github.com>
2020-05-31 18:52:03 +0200

    added a vue

--- src/main.js
+++ src/main.js
@@ -1,32 +1,47 @@
 // API KEY: abb493012a5a47909c682c381a8e855b
-var url = 'http://newsapi.org/v2/top-headlines?' +
-          'country=us&' +
-          'apiKey=abb493012a5a47909c682c381a8e855b';
+let url = 'http://newsapi.org/v2/top-headlines?pageSize=50&';
 
-// const vm = new Vue({
-// el: '#app',
-// data: {
-//     results: [
-//         {title: "the very first post", abstract: "lorem ipsum some test dimpsum"},
-//         {title: "and then there was the second", abstract: "lorem ipsum some test dimsum"},
-//         {title: "third time's a charm", abstract: "lorem ipsum some test dimsum"},
-//         {title: "four the last time", abstract: "lorem ipsum some test dimsum"}
-//         ]
-//     }
-// });
-
+let country = 'country=se&';
+let api = 'apiKey=abb493012a5a47909c682c381a8e855b';
 
 const app = new Vue({
     el: '#app',
     data: {
-        articles: []
+        articles: [],
     },
     
     mounted: function() {
-        fetch(url)
+        fetch(url + country + api)
             .then(response => response.json())
             .then(articlesResponse => {
                 this.articles = articlesResponse.articles
             })
+    }
+})
+
+Vue.component('search-button', {
+    data: function () {
+      return {
+        inputCategory: "",
+        inputCountry: "",
+        inputPhrase: ""
+      }
+    },
+    template: '<button @click="onSearch" class="button"><i class="fas fa-search"></button>',
+    methods: {
+        onSearch() {
+            const inputCathegory = this.inputCategory;
+            const inputCountry = this.inputCountry;
+            const inputPhrase = this.inputPhrase;
+
+            fetch (url + 'country=' + this.inputCountry + '&' + 'category=' + this.inputCategory + '&' + 'q=' + this.inputPhrase + api)
+                .then(response => response.json())
+                .then(articlesResponse => {
+                    this.articles = articlesResponse.articles
+                })
+        },
+
+        
+
     }
 })
