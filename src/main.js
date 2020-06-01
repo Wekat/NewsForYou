883da6f80c549491e33db65d95c3dd2a586826f2
Wekat <60980395+Wekat@users.noreply.github.com>
2020-06-01 17:57:53 +0200

    Some mixtering

--- src/main.js
+++ src/main.js
@@ -31,12 +31,33 @@ const app = new Vue({
             currentPage: 1,
             maxPerPage: 50,
             inputCategory: '',
-            inputCountry: '',
+            inputCountry: 'se',
             inputPhrase:'',
             articles: [],
         }
     },
+
+    computed: {
+        apiUrl() {
+            if (this.inputCategory !== '' && this.inputCountry !== '' && this.inputPhrase !== '') {
+                this.apiUrl = url + `category=${this.inputCategory}&`
+                + `country=${this.inputCountry}&`
+                + `q=${this.inputPhrase}&` + api;
+            } else if (this.inputCategory !== '') {
+                this.apiUrl = url + `country=${this.inputCountry}&`
+                + `q=${this.inputPhrase}&` + api;
+            } else if (this.inputCountry !== '') {
+                this.apiUrl = url + `category=${this.inputCategory}&`
+                + `q=${this.inputPhrase}&` + api;
+            } else if (this.inputPhrase !== '') {
+                this.apiUrl = url + `category=${this.inputCategory}&`
+                + `country=${this.inputCountry}&`
+                + api;
+            }
+        },
+    },
     
+    // denna vill kunna ha bara genom anrop på fetchData!
     mounted: function() {
         fetch(url + country + api)
             .then(response => response.json())
@@ -46,99 +67,108 @@ const app = new Vue({
     },
 
     methods: {
-        resetData() {
-            this.currentPage = 1;
-            this.articles = [];
-        },
-        fetchSearchNews() {
-            if(this.inputCategory !== '' || this.inputCountry !== '' || this.inputPhrase !== '') {
-                this.apiUrl = url + country + api;
-                this.isBusy = true;
-
-                this.resetData();
-                this.fetachData();
-            }
-            else {
-                this.fetchTopNews();
-            }
-            
-        },
-        fetchTopNews() {
-            this.apiUrl = url + 'category=' + this.inputCategory + '&'
-            + 'country=' + this.inputCountry + '&'
-            + 'q=' + this.inputPhrase + '&' + api;
-
-            this.isBusy = true;
-            this.inputCategory = '';
-            this.inputCountry = '';
-            this.inputPhrase = '';
-
+        onSearch() {
             this.resetData();
             this.fetchData();
         },
+
+        resetData() {
+            this.articles = [];
+        },
+        // fetchSearchNews() {
+        //     if(this.inputCategory !== '' || this.inputCountry !== '' || this.inputPhrase !== '') {
+        //         this.apiUrl = url + country + api;
+        //         this.isBusy = true;
+
+        //         this.resetData();
+        //         this.fetachData();
+        //     }
+        //     else {
+        //         this.fetchTopNews();
+        //     }
+            
+        // },
+        // fetchTopNews() {
+        //     this.apiUrl = url + 'category=' + this.inputCategory + '&'
+        //     + 'country=' + this.inputCountry + '&'
+        //     + 'q=' + this.inputPhrase + '&' + api;
+
+        //     this.isBusy = true;
+        //     this.inputCategory = '';
+        //     this.inputCountry = '';
+        //     this.inputPhrase = '';
+
+        //     this.resetData();
+        //     this.fetchData();
+        // },
         fetchData() {
-            let req = new Request(this.apiUrl + '&page=' + this.currentPage);
+            let req = new Request(this.apiUrl);
             fetch(req)
-                .then((resp) => resp.json())
-                .then((data) => {
-                    this.totalResults = data.totalResults;
-                    data.articles.forEach(element => {
-                        this.article.push(element);
-                    });
-                    this.isBusy = false;
-                    this.showLoader = false;
+                // .then((resp) => resp.json())
+                // .then((data) => {
+                //     this.totalResults = data.totalResults;
+                //     data.articles.forEach(element => {
+                //         this.articles.push(element);
+                //     });
+                //     this.isBusy = false;
+                //     this.showLoader = false;
+
+                .then(response => response.json())
+                .then(articlesResponse => {
+                    this.articles = articlesResponse.articles
                 })
+
                 .catch((error) => {
                     Console.log(error);
                 })
         },
-        created() {
-            this.fetachTopNews();
+        mounted() {
+            this.fetchData();
         }
     }
 })
 
-Vue.component('search-button', {
-    data: function () {
-      return {
-        inputCategory: '',
-        inputCountry: '',
-        inputPhrase: ''
-      }
-    },
-    template: '<button @click="fetchSearchNews" class="button"><i class="fas fa-search"></button>',
-    methods: {
-        fetchSearchNews() {
-            if(this.inputCategory !== '' || this.inputCountry !== '' || this.inputPhrase !== '') {
-                this.apiUrl = url + country + api;
-                this.isBusy = true;
+// Vue.component('search-button', {
+//     data() {
+//       return {
+//         inputCategory: '',
+//         inputCountry: '',
+//         inputPhrase: ''
+//       }
+//     },
+//     template: '<button @click="fetchSearchNews" class="button"><i class="fas fa-search"></i></button>',
+//     methods: {
+//         fetchSearchNews() {
+//             if(this.inputCategory !== '' || this.inputCountry !== '' || this.inputPhrase !== '') {
+//                 this.apiUrl = url + country + api;
+//                 this.isBusy = true;
 
-                this.resetData();
-                this.fetachData();
-            }
-            else {
-                this.fetchTopNews();
-            }
+//                 this.resetData();
+//                 this.fetchData();
+//             }
+//             else {
+//                 this.fetchTopNews();
+//             }
             
-        },
-        fetchTopNews() {
-            this.apiUrl = url + 'category=' + this.inputCategory + '&'
-            + 'country=' + this.inputCountry + '&'
-            + 'q=' + this.inputPhrase + '&' + api;
+//         },
+//         fetchTopNews() {
+//             this.apiUrl = url + 'category=' + this.inputCategory + '&'
+//             + 'country=' + this.inputCountry + '&'
+//             + 'q=' + this.inputPhrase + '&' + api;
 
-            this.isBusy = true;
-            this.inputCategory = '';
-            this.inputCountry = '';
-            this.inputPhrase = '';
+//             this.isBusy = true;
+//             this.inputCategory = '';
+//             this.inputCountry = '';
+//             this.inputPhrase = '';
 
-            this.resetData();
-            this.fetchData();
-        },
+//             this.resetData();
+//             this.fetchData();
+//         },
 
         
 
-    }
-})
+//     }
+// })
 
 // Vue.component('search-button', {
 //     data: function () {
